fb13ece807feeb22db3a18c467984b10d94b3565
Safdhar10 <safdhard@gmail.com>
2021-03-10 11:47:22 +0530

    PRoblem

---
+++ AtCoderContest/WelcomeToAtCoder.java
@@ -0,0 +1,15 @@
+package AtCoderContest;
+
+import java.util.Scanner;
+
+public class WelcomeToAtCoder {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+        int c=scan.nextInt();
+        String s=scan.next();
+        System.out.println((a+b+c)+" "+s);
+    }
+    
+}
