4a395edc86778ba33150aa69220a541430722bb8
Safdhar R <68153873+Safdhar10@users.noreply.github.com>
2021-06-07 10:56:37 +0530

    Atcoder Accept PRblm

---
+++ BeginnerContest(June 6)/RockPaper.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+/**
+ * RockPaper
+ */
+public class RockPaper {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+
+        String[] _inputArray=sc.nextLine().split(" ");
+
+        int FennectThrown=Integer.parseInt(_inputArray[0]);
+        int RaccoonThrown=Integer.parseInt(_inputArray[1]);
+
+        if(FennectThrown!=RaccoonThrown)
+        {
+            System.out.println(3-(FennectThrown+RaccoonThrown));
+        }
+        else
+        {
+            System.out.println(FennectThrown);
+        }
+    }
+}
