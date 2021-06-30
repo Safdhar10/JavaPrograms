74591633aa697ce06d6e7933b512b7397a238190
Safdhar10 <safdhard@gmail.com>
2021-06-30 12:31:01 +0530

    prblm

---
+++ HackerRank/SumofArray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class SumofArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int result=0;
+        for(int i=0;i<n;i++)
+        {
+            result+=sc.nextInt();
+        }
+        System.out.println(result);
+    }
+}
