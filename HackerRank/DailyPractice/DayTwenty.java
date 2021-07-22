1c4fa10ed2e0a4c6a5d496618da32a503efa08e4
Safdhar10 <safdhard@gmail.com>
2021-07-22 12:14:08 +0530

    20 prblm

---
+++ HackerRank/DailyPractice/DayTwenty.java
@@ -0,0 +1,55 @@
+package DailyPractice;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class DayTwenty {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] aTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int[] a=new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aTemp[i]);
+            a[i]=aItem;
+        }
+        int totalSwaps=0;
+        // Write your code here
+        for (int i = 0; i < n; i++) {
+            // Track number of elements swapped during a single array traversal
+            int numberOfSwaps = 0;
+            
+            for (int j = 0; j < n - 1; j++) {
+                // Swap adjacent elements if they are in decreasing order
+                if (a[j] > a[j + 1]) {
+                    int temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                    numberOfSwaps++;
+                    totalSwaps++;
+                }
+            }
+            
+            // If no elements were swapped during a traversal, array is sorted
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+        bufferedReader.close();
+        
+        System.out.println("Array is sorted in "+totalSwaps+" swaps.");
+        System.out.println("First Element: "+a[0]);
+        System.out.println("Last Element: "+a[n-1]);
+    }
+}
